f396246826e469c2000806068716bf05b451b0a3
fabien.vorpe <fabien.vorpe@he-arc.ch>
2019-04-27 13:26:49 +0200

    started the base for the api server

---
+++ aJeannedArc/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aJeannedArc.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String Firstname { get; set; }
+        public String Lastname { get; set; }
+    }
+}
--- aJeannedArc/Startup.cs
+++ aJeannedArc/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using aJeannedArc.Models;
 
 namespace aJeannedArc
 {
@@ -20,6 +22,7 @@ namespace aJeannedArc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("aJeannedArc"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
