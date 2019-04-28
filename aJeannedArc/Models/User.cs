287e91a1a349fe8c672b0224f357a1f28ced3fd9
fabien.vorpe <fabien.vorpe@he-arc.ch>
2019-04-28 17:44:11 +0200

    add login method

--- aJeannedArc/Models/User.cs
+++ aJeannedArc/Models/User.cs
@@ -11,5 +11,6 @@ namespace aJeannedArc.Models
         public String Username { get; set; }
         public String Email { get; set; }
         public String Password { get; set; }
+        public byte[] Salt { get; set; }
     }
 }
